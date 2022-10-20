b8f3e1f6e2957ccc5027110e6237159768e7a24c
Diego Galindo <diegogalindo84@gmail.com>
2022-10-20 20:17:53 -0300

    first commit

---
+++ src/main/java/com/mycompany/calculadora/Principal.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+import com.mycompany.calculadora.visao.JFrTela;
+
+/**
+ *
+ * @author Diego
+ */
+public class Principal {
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new JFrTela().setVisible(true);
+        });
+    }
+}
